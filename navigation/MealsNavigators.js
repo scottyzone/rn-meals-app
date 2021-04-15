e963ee247f8b0b1073b07735c76cf52aae85664f
Scott Owen <sowamadeo@gmail.com>
2021-04-15 17:18:52 +0800

    Passing Data to the Meal Detail Screen

---
+++ navigation/MealsNavigators.js
@@ -0,0 +1,32 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
+
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
+
+import Colors from '../constants/colors';
+
+const MealsNavigator = createStackNavigator({
+    Categories: {
+        screen: CategoriesScreen,          
+        navigationOptions: {
+            headerTitle: 'Meal Categories',
+        }
+    },
+    CategoryMeals: {
+        screen: CategoryMealsScreen,
+    },
+    MealDetail: MealDetailScreen
+}, {    
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+        headerTitle: 'A Screen',
+    }
+});
+
+export default createAppContainer(MealsNavigator);
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+
+const CategoriesScreen = props => {
+
+    const renderGridItem = (itemData) => {
+        return (
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color}
+                onSelect={() => {
+                    props.navigation.navigate({routeName: 'CategoryMeals', 
+                    params: {
+                        categoryId: itemData.item.id,
+                    }
+                    });
+                    }
+                }
+        />
+        );
+                
+    };
+
+    return (
+        <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
+    }
+});
+
+export default CategoriesScreen;
